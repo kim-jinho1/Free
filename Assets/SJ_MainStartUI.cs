3e2041a2b6431c511b1ee1fdcf307f751c2dfd2b
djEjsdlfma <asdakpol.asd@gmail.com>
2024-12-23 09:48:09 +0900

    feat : Start
    시작씬 에셋 넣음

--- Assets/SJ_MainStartUI.cs
+++ Assets/SJ_MainStartUI.cs
@@ -7,11 +7,11 @@ using TMPro;
 public class SJ_MainStartUI : MonoBehaviour
 {
     [SerializeField] private GameObject BtnGroup;
-    [SerializeField] private TextMeshProUGUI Title;
+    [SerializeField] private GameObject Title;
 
     void Start()
     {
         BtnGroup.transform.DOMove(new Vector3(1003,540), 1.5f).SetEase(Ease.OutBack);
-        Title.transform.DOMove(new Vector3(657,893), 1.2f).SetEase(Ease.OutBack);
+        Title.transform.DOMove(new Vector3(600,893), 1.2f).SetEase(Ease.OutBack);
     }
 }

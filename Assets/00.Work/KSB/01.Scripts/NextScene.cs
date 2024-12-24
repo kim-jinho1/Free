fe9530283b3a0dac8e94ed5dbae71564808eebfb
sobinkim08 <157638604+sobinkim@users.noreply.github.com>
2024-12-24 11:41:05 +0900

    start CutScene

---
+++ Assets/00.Work/KSB/01.Scripts/NextScene.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class NextScene : MonoBehaviour
+{
+
+    [SerializeField] private SceneAsset scene; 
+    [SerializeField] private string sceneName;
+
+    void Start()
+    {
+
+        if (scene != null)
+        {
+            sceneName = scene.name;
+        }
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(sceneName); // 씬 이름으로 전환
+        }
+    }
+}

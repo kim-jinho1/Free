e60b89b996360b8362ecd576ebcdd241e83e05ed
김진호 <russiakim2008@gmail.com>
2024-12-26 11:12:58 +0900

    fix : MainScene

--- Assets/00.Work/KSB/01.Scripts/NextScene.cs
+++ Assets/00.Work/KSB/01.Scripts/NextScene.cs
@@ -5,27 +5,7 @@ using UnityEditor;
 
 public class NextScene : MonoBehaviour
 {
-    [SerializeField] private SceneAsset scene; 
     [SerializeField] private string sceneName;
-
-    void Start()
-    {
-
-        if (scene != null)
-        {
-            sceneName = scene.name;
-        }
-
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            MoveNextScene(); // 씬 이름으로 전환
-        }
-    }
-
     public void MoveNextScene()
     {
         SceneManager.LoadScene("MainGameScene");
